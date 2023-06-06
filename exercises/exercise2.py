9f0970b3c2a6e160c7197019579f4dc30647a0ef
Soham Basu <buzztosoham@gmail.com>
2023-06-06 17:29:52 +0200

    changes in datatypes

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,53 +1,26 @@
-import pandas as pd
 from sqlalchemy import create_engine
+import pandas as pd
 
-# Fetch the CSV data from the source URL
-data = pd.read_csv('https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV', sep=';', decimal=',')
-
-# Define the desired data types
-dtypes = {'EVA_NR': 'int', 'DS100': 'object', 'IFOPT': 'object', 'NAME': 'object', 'Verkehr': 'object', 'Laenge': 'float', 'Breite': 'float', 'Betreiber_Name': 'object', 'Betreiber_Nr': 'float'}
-
-# Convert the data types
-data = data.astype(dtypes)
+# Read data
+data_url = 'https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV'
+data_df = pd.read_csv(data_url, sep=';', decimal=',')
 
 # First, drop the "Status" column
-data = data.drop('Status', axis=1)
+data_df = data_df.drop(['Status'], axis=1)
 
-# Drop rows with invalid "Verkehr" values
-valid_verkehr_values = ["FV", "RV", "nur DPN"]
-data = data[data["Verkehr"].isin(valid_verkehr_values)]
+# Drop rows with invalid values
+valid_verkehr = ["FV", "RV", "nur DPN"]
+data_df = data_df[data_df['Verkehr'].isin(valid_verkehr)]
 
-# Remove rows with invalid coordinate values
-data = data[(data['Laenge'] >= -90) & (data['Laenge'] <= 90)]
-data = data[(data['Breite'] >= -90) & (data['Breite'] <= 90)]
+data_df = data_df[(data_df['Laenge'] >= -90) & (data_df['Laenge'] <= 90) & (data_df['Breite'] >= -90) & (data_df['Breite'] <= 90)]
 
-# Define the regex pattern for valid "IFOPT" values
-pattern = r'^[A-Za-z]{2}:\d+:\d+(?::\d+)?$'
-data = data[data['IFOPT'].str.contains(pattern, na=False)]
+data_df = data_df[data_df['IFOPT'].str.contains(r'^[A-Za-z]{2}:\d*:\d*(?::\d*)?$', na=False)]
 
-# Drop rows with empty cells
-data = data.dropna()
+data_df = data_df.dropna()
 
-# Create a SQLite engine using SQLAlchemy
-engine = create_engine('sqlite:///trainstops.sqlite')
+# Convert "Betreiber_Nr" column to integer
+data_df["Betreiber_Nr"] = data_df["Betreiber_Nr"].astype(int)
 
-# Create the table with appropriate SQLite types
-table_query = '''
-    CREATE TABLE trainstops (
-        EVA_NR INTEGER,
-        DS100 TEXT,
-        IFOPT TEXT,
-        NAME TEXT,
-        Verkehr TEXT,
-        Laenge REAL,
-        Breite REAL,
-        Betreiber_Name TEXT,
-        Betreiber_Nr REAL
-    )
-'''
-with engine.connect() as connection:
-    connection.execute(table_query)
-    data.to_sql('trainstops', connection, if_exists='replace', index=False)
-
-# Close the engine
-engine.dispose()
+# Create the engine without printing execution details
+engine = create_engine("sqlite:///trainstops.sqlite", echo=False)
+data_df.to_sql('trainstops', engine, if_exists='replace', index=False)
