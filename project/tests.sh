6ae83c53fae92fbec46361ce0a948c8bd97b7578
Soham Basu <buzztosoham@gmail.com>
2023-06-13 19:40:15 +0200

    Create tests.sh

---
+++ project/tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh  
+
+# execute the pipeline
+echo "Execute the pipeline"
+python data/pipeline.py
+
+#!/bin/sh  
+
+# execute the pipeline
+echo "Execute the pipeline"
+python data/test.py
